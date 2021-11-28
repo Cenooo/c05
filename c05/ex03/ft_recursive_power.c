83e254e011dd11dca5bc0689b8c60b0233a22466
Cenooo <94233626+Cenooo@users.noreply.github.com>
2021-11-28 15:13:41 +0100

    Add files via upload

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpolat <cpolat@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 15:04:35 by cpolat            #+#    #+#             */
+/*   Updated: 2021/11/28 15:05:53 by cpolat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else if (power > 0)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	return (nb);
+}
---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpolat <cpolat@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 15:06:49 by cpolat            #+#    #+#             */
+/*   Updated: 2021/11/28 15:07:40 by cpolat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		return (0);
+	}
+	else if (index == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	}
+	return (0);
+}
